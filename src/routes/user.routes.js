11f896f127a1930e1e57478c90280ad882457ac2
Salam wDev <salam.ads97@gmail.com>
2024-08-30 00:45:56 +0600

    add refreshAccessToken endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -27,5 +27,6 @@ router.route("/login").post(loginUser);
 
 // Secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
