bc450d6b016a98e7384e8ade5f078329db9e6902
Salam wDev <salam.ads97@gmail.com>
2024-09-04 20:32:26 +0600

    add method for getting user channel profile

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const subscriptionSchema = new Schema(
   {
-    subscription: {
+    subscriber: {
       type: Schema.Types.ObjectId, // one who is subscribing
       ref: "User",
     },
